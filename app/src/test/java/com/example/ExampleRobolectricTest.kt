f72cec744f3af9e996f22f85a2a62ddf89919d3a
youbrey <138248101+youbrey@users.noreply.github.com>
2026-08-01 10:40:46 +0800

    Add files via upload

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -9,7 +9,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class ExampleRobolectricTest {
 
   @Test
